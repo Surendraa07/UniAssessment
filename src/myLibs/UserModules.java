de70e21ec11521574b2388ffa30cb2e67b6bf55b
UpalKarki <karkiupal29@gmail.com>
2025-04-13 12:58:13 +0545

    Update UserModules.java

--- src/myLibs/UserModules.java
+++ src/myLibs/UserModules.java
@@ -1,45 +1,39 @@
 package myLibs;
 
 public class UserModules {
+	// fields to store
 	private int userId;
 	private String password;
 
-	//Constructors, Setters, Getter, and toString
-	
-	//Default Constructor
-	public UserModules(int userId, String password) {
+	public UserModules(int userId, String password) {//Parameterized constructor
 		this.userId = userId;
 		this.password = password;
 	}
-	
-	//Parameterized Constructor
-	public UserModules() {
+
+	public UserModules() {//Default constructor
+
 		this.userId = 0;
 		this.password = "";
 	}
 
-	//Copy Constructor
-	public UserModules(UserModules userModules) {
+	public UserModules(UserModules userModules) {//Copy constructor
+
 		this.userId = userModules.userId;
 		this.password = userModules.password;
 	}
-
-	//Getters
+	//getters and setters
 	public int getUserId() {
 		return userId;
 	}
 
-	//Setters
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 
-	//Getters
 	public String getPassword() {
 		return password;
 	}
 
-	//Setters
 	public void setPassword(String password) {
 		this.password = password;
 	}
