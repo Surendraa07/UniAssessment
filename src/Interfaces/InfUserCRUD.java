0c63a62375f2ee5a53e8ad6c8e9dbb76219a5fa3
UpalKarki <karkiupal29@gmail.com>
2025-04-13 13:00:33 +0545

    Update InfUserCRUD.java

--- src/Interfaces/InfUserCRUD.java
+++ src/Interfaces/InfUserCRUD.java
@@ -1,11 +1,13 @@
 package Interfaces;
-
 import myLibs.RegularUserModules;
+import myLibs.TrainerModules;
 import myLibs.UserDetailsModules;
 import myLibs.UserModules;
+
 
 public interface InfUserCRUD {
 	public boolean Insert(UserModules userModule);
 	public boolean Insert(UserDetailsModules userDetailsModules);
 	public boolean Insert(RegularUserModules regularUserModules);
+	public boolean Insert(TrainerModules trainerModules);
 }
