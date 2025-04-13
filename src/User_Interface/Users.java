aa4f80f6bb469cef0ffeeffb2e33c7595e01f147
UpalKarki <karkiupal29@gmail.com>
2025-04-13 12:46:07 +0545

    Update Users.java

--- src/User_Interface/Users.java
+++ src/User_Interface/Users.java
@@ -1,106 +1,124 @@
-package User_Interface;
+package User_interface;
 
+// Import required JavaFX components and user-defined modules
 import Repo.UsersRepo;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Stop;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import myLibs.UserModules;
-
 public class Users extends Application {
+
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-		//Declare
-		Label lblTitle, lblUserId, lblPassword;
-		TextField txtUserId, txtPassword;
-		CheckBox chkLoggedIn, chkLoggedOut;
-		Button btnSubmit, btnClose;
+	public void start(Stage primaryStage) {
+		// Create the root pane 
+		Pane root = new Pane();
 
-		//Initialize
-		lblTitle = new Label("Health & Fitness");
-		lblTitle.relocate(170, 30);
-		Font font1 = new Font("Arial", 20);
-		lblTitle.setFont(font1);
+		// Set gradient background color using CSS style
+		Stop[] stops = new Stop[] {
+			new Stop(0, Color.web("#E6EFE9")),
+			new Stop(1, Color.web("#7F8C8D"))
+		};
+		root.setStyle("-fx-background-color: linear-gradient(to right, #E6EFE9, #7F8C8D);");
 
-		lblUserId = new Label("USERID");
-		lblUserId.relocate(100, 90);
-		Font font2 = new Font("Arial", 14);
-		lblUserId.setFont(font2);
+		// Title label for the application
+		Label lblTitle = new Label("Health & Fitness");
+		lblTitle.setFont(new Font("Serif", 26));
+		lblTitle.setStyle("-fx-background-color: #899D89; -fx-text-fill: white; -fx-padding: 10; -fx-background-radius: 20;");
+		lblTitle.relocate(140, 20); 
+		lblTitle.setPrefSize(220, 50);
+		lblTitle.setAlignment(javafx.geometry.Pos.CENTER); 
 
-		txtUserId = new TextField();
-		txtUserId.relocate(170, 85);
+		// Label and TextField 
+		Label lblUserId = new Label("USERID");
+		lblUserId.setFont(new Font("Arial", 16));
+		lblUserId.setStyle("-fx-font-weight: bold;");
+		lblUserId.relocate(90, 90);
 
-		lblPassword = new Label("PASSWORD");
-		lblPassword.relocate(75, 140);
-		lblPassword.setFont(font2);
+		TextField txtUserId = new TextField();
+		txtUserId.relocate(180, 85);
+		txtUserId.setPrefSize(180, 30);
+		txtUserId.setStyle("-fx-background-color: #DCE8D9; -fx-background-radius: 15;");
 
-		txtPassword = new TextField();
-		txtPassword.relocate(170, 135);
+		Label lblPassword = new Label("PASSWORD");
+		lblPassword.setFont(new Font("Arial", 16));
+		lblPassword.setStyle("-fx-font-weight: bold;");
+		lblPassword.relocate(80, 140);
 
-		// Login Status
+		TextField txtPassword = new TextField();
+		txtPassword.relocate(180, 135);
+		txtPassword.setPrefSize(180, 30);
+		txtPassword.setStyle("-fx-background-color: #DCE8D9; -fx-background-radius: 15;");
+
 		Label lblLoginStatus = new Label("Login Status");
-		lblLoginStatus.relocate(100, 190);
-		lblLoginStatus.setFont(font2);
-
-		chkLoggedIn = new CheckBox("Logged IN");
+		lblLoginStatus.setFont(new Font("Arial", 16));
+		lblLoginStatus.setStyle("-fx-font-weight: bold;");
+		lblLoginStatus.relocate(90, 190);
+		// CheckBox for logged in and logged out 
+		CheckBox chkLoggedIn = new CheckBox("Logged IN");
 		chkLoggedIn.relocate(120, 220);
 
-		chkLoggedOut = new CheckBox("Logged Out");
+		CheckBox chkLoggedOut = new CheckBox("Logged Out");
 		chkLoggedOut.relocate(250, 220);
+		chkLoggedOut.setSelected(true);
 
-		chkLoggedOut.setSelected(true); // Default selection
-
-		// Buttons
-		btnSubmit = new Button("Submit");
-		btnSubmit.relocate(140, 250);
+		// Submit button to handle user data submission
+		Button btnSubmit = new Button("Submit");
+		btnSubmit.relocate(140, 270);
 		btnSubmit.setPrefSize(80, 30);
-		btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
+		btnSubmit.setStyle("-fx-background-color: black; -fx-text-fill: white;");
 
+		// Event handler for the Submit button
+		btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent actionEvent) {
 				UserModules users = new UserModules();
 				users.setUserId(Integer.parseInt(txtUserId.getText()));
 				users.setPassword(txtPassword.getText());
+
 				boolean result = new UsersRepo().Insert(users);
+
+				UserDetails userDetails = new UserDetails();
+				Stage userDetailsStage = new Stage();
+				try {
+					userDetails.start(userDetailsStage);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
 
-		btnClose = new Button("Close");
-		btnClose.relocate(250, 250);
+		// Close button to exit the application
+		Button btnClose = new Button("Close");
+		btnClose.relocate(250, 270);
 		btnClose.setPrefSize(80, 30);
+		btnClose.setStyle("-fx-background-color: black; -fx-text-fill: white;");
 		btnClose.setOnMouseClicked((new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent event) {
 				primaryStage.close();
 			}
 		}));
-		//Add Controls
-		Pane pane = new Pane();
-		Scene scene = new Scene(pane);
-		primaryStage.setScene(scene);
-		primaryStage.setWidth(500);
-		primaryStage.setHeight(400);
-		primaryStage.setTitle("Users Form");
-		
-		pane.getChildren().addAll(lblTitle, lblUserId, lblPassword, lblLoginStatus);
-		pane.getChildren().addAll(txtUserId, txtPassword);
-		pane.getChildren().addAll(chkLoggedIn, chkLoggedOut);
-		pane.getChildren().addAll(btnSubmit, btnClose);
-		// display window
-		primaryStage.show();
 
+		// Add all components to the pane
+		root.getChildren().addAll(lblTitle,lblUserId, txtUserId,lblPassword, txtPassword,lblLoginStatus, chkLoggedIn, chkLoggedOut,btnSubmit, btnClose);
+
+		// Create the scene, set the stage, and show the window
+		Scene scene = new Scene(root, 500, 400);
+		primaryStage.setTitle("Users Login");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 }
+
